d2731527826fca1df94e1ede2ef5b0039d5eda9e
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-28 07:20:45 +0530

    Update Test_Save.java

--- com/app/test/Test_Save.java
+++ com/app/test/Test_Save.java
@@ -11,7 +11,7 @@ import com.app.entity.Employee;
 public class Test_Save {
 
 	public static void main(String[] args) {
-	/*	//get the spring configuration
+		//get the spring configuration
 		AnnotationConfigApplicationContext ctx=new AnnotationConfigApplicationContext(SpringConfig.class);
 		//get the hibernate Template bean
 		HibernateTemplate ht=(HibernateTemplate)ctx.getBean("htObj");
@@ -22,12 +22,8 @@ public class Test_Save {
 		e.setEmpsal(20000);
 		//save the object
 		int id=(Integer)ht.save(e);
-			System.out.println("\n\t\tEmployee Saved with ID: "+id);*/
-		SpringConfig sc=new SpringConfig();
-		
-		System.out.println(sc.dsObj());
-		System.out.println(sc.sfObj());
-	//	System.out.println(sc.htObj());
+			System.out.println("\n\t\tEmployee Saved with ID: "+id);
+
 	}
 
 }
